8f20e9761e8114f59d88cd2361e7467a79f32a1d
fraolb <fraolbereket@gmail.com>
2024-08-23 17:42:56 +0300

    add initialize to proxy

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    /// @custom: oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializer();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function initialize() public initializer {}
 
     function getNumber() external view returns (uint256) {
         return number;
