e355d27374e8073e67df46d1f74816070ce0d36c
fraolb <fraolbereket@gmail.com>
2024-09-09 12:09:23 +0300

    box v2

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,18 +1,36 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract BoxV2 {
-    uint256 internal number;
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-    function setNumber(uint256 _number) external {
-        number = _number;
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal value;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    function getNumber() external view returns (uint256) {
-        return number;
+    function initialize() public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
     }
 
-    function version() external pure returns (uint256) {
+    function setValue(uint256 newValue) public {
+        value = newValue;
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+
+    function version() public pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
